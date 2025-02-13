cc0ac20926820efd99931816690883726a35585c
jatin <adriano23012001@gmail.com>
2025-02-13 15:58:35 +0530

    Added SecurityConfig

---
+++ src/main/java/com/pluuginstore/brand_analytics/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.pluuginstore.brand_analytics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated() // Secure everything else
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .httpBasic(withDefaults()); // Basic authentication
+
+        return http.build();
+    }
+}
