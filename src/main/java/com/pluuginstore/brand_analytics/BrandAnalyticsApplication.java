d9d505ca4c8e3b583cfab166bec545839c78a94c
jatin <adriano23012001@gmail.com>
2025-02-13 15:44:24 +0530

    Backend Setup

---
+++ src/main/java/com/pluuginstore/brand_analytics/BrandAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.pluuginstore.brand_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluuginstore/brand_analytics/BrandAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluuginstore.brand_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
