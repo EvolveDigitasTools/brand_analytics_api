e1442f2f7073a4e06271b52bf2fcefd96cf1949c
jatin <adriano23012001@gmail.com>
2025-03-28 12:15:09 +0530

    added VendorEntity with repository

---
+++ src/main/java/com/pluuginstore/brand_analytics/entity/VendorEntity.java
@@ -0,0 +1,34 @@
+package com.pluuginstore.brand_analytics.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "vendor",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "vendorCode"),
+                @UniqueConstraint(columnNames = "companyName")
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VendorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String vendorCode;
+
+    @Column(nullable = false)
+    private String productCategory;
+
+    @Column(nullable = false, unique = true)
+    private String companyName;
+
+    @OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SKUEntity> skus;
+}
--- src/main/java/com/pluuginstore/brand_analytics/repository/VendorRepository.java
+++ src/main/java/com/pluuginstore/brand_analytics/repository/VendorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BrandRepository extends JpaRepository<OrderItemEntity, Long> {
+public interface VendorRepository extends JpaRepository<OrderItemEntity, Integer> {
 }
